f29da4c8635b8dc49837a31aad8816a9da47abd9
deepakgarg <dgarg611@gmail.com>
2025-02-12 00:06:37 +0530

    ititial commit

---
+++ fileupload-ui/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello this a react
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
